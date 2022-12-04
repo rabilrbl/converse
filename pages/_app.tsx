dc6585c6772b711ac56cfc08a7873b2a86b4d245
Mohammed Rabil <rabil@techie.com>
2022-12-04 13:57:46 +0530

    Setup tailwindcss and add theme switcher

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,44 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import { AppProps } from "next/app";
+import Head from "next/head";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import "../styles/globals.css";
+import { useState } from "react";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
   return (
     <>
       <Head>
         <title>Converse</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'dark',
-        }}
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <Component {...pageProps} />
-      </MantineProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: colorScheme,
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,47 @@
-import Image from "next/image";
+import {
+  ActionIcon,
+  Button,
+  Group,
+  Switch,
+  useMantineColorScheme,
+  useMantineTheme,
+} from "@mantine/core";
 import Meta from "../components/Common/Meta";
+import { IconSun, IconMoonStars } from "@tabler/icons";
 
 export default function Home() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const theme = useMantineTheme();
   return (
-    <div className="p-4 bg-blue-500">
+    <div className="">
       <Meta
         title="Converse"
         description="Converse is a chat app built with Next.js and Mantine."
       />
 
-      <main></main>
+      <main>
+        <Group position="center" my={30}>
+          <Switch
+            checked={colorScheme === "dark"}
+            onChange={() => toggleColorScheme()}
+            size="lg"
+            onLabel={<IconSun color={theme.white} size={20} stroke={1.5} />}
+            offLabel={
+              <IconMoonStars
+                color={theme.colors.gray[6]}
+                size={20}
+                stroke={1.5}
+              />
+            }
+          />
+        </Group>
+      </main>
 
-      <footer>Footer</footer>
+      <footer className="p-4">
+        <Button className="" variant="outline">
+          Footer
+        </Button>
+      </footer>
     </div>
   );
 }
