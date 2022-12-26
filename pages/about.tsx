7acf683489bffd457f944b101be6a7d7336f2b00
Mohammed Rabil <rabil@techie.com>
2022-12-26 13:10:16 +0530

    Add about page on project

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import { createStyles, Container, Text, Button, Group } from '@mantine/core';
+import { GithubIcon } from '@mantine/ds';
 
-const about = () => {
+const BREAKPOINT = '@media (max-width: 755px)';
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    position: 'relative',
+    boxSizing: 'border-box',
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+  },
+
+  inner: {
+    position: 'relative',
+    paddingTop: 200,
+    paddingBottom: 120,
+
+    [BREAKPOINT]: {
+      paddingBottom: 80,
+      paddingTop: 80,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 62,
+    fontWeight: 900,
+    lineHeight: 1.1,
+    margin: 0,
+    padding: 0,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+    [BREAKPOINT]: {
+      fontSize: 42,
+      lineHeight: 1.2,
+    },
+  },
+
+  description: {
+    marginTop: theme.spacing.xl,
+    fontSize: 24,
+
+    [BREAKPOINT]: {
+      fontSize: 18,
+    },
+  },
+
+  controls: {
+    marginTop: theme.spacing.xl * 2,
+
+    [BREAKPOINT]: {
+      marginTop: theme.spacing.xl,
+    },
+  },
+
+  control: {
+    height: 54,
+    paddingLeft: 38,
+    paddingRight: 38,
+
+    [BREAKPOINT]: {
+      height: 54,
+      paddingLeft: 18,
+      paddingRight: 18,
+      flex: 1,
+    },
+  },
+}));
+
+export default function About() {
+  const { classes } = useStyles();
+
   return (
-    <div>about</div>
-  )
-}
+    <div className={classes.wrapper+" h-screen"}>
+      <Container className={classes.inner}>
+        <h1 className={classes.title}>
+          <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
+            Converse
+          </Text>{' '}
+          is an SJEC student forum to communicate with your peers.
+        </h1>
 
-export default about
+        <Text className={classes.description} color="dimmed">
+          Converse is a student forum for SJEC students to communicate with each other. It is a place to share your thoughts, ideas, and opinions with your peers. It is a place to share your thoughts, ideas, and opinions with your peers.
+        </Text>
+
+        <Group className={classes.controls}>
+          <Button
+            size="xl"
+            className={classes.control}
+            variant="outline"
+            gradient={{ from: 'blue', to: 'cyan' }}
+          >
+            Explore
+          </Button>
+
+          <Button
+            component="a"
+            href="https://github.com/rabilrbl/converse"
+            size="xl"
+            variant="default"
+            className={classes.control}
+            leftIcon={<GithubIcon size={20} />}
+          >
+            GitHub
+          </Button>
+        </Group>
+      </Container>
+    </div>
+  );
+}
