2313c97f1047a744364be5433da2aaae938a9630
Mohammed Rabil <rabil@techie.com>
2023-01-16 01:03:37 +0530

    Use frontend api call for posts page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,24 +2,18 @@ import { Button, Container, Flex, Title } from "@mantine/core";
 import { IconPlus } from "@tabler/icons";
 import { useRouter } from "next/router";
 import { ArticleCardVertical } from "../../components/Posts/ArticleCard";
-import { PrismaClient } from "@prisma/client";
+import useSWR from "swr";
+import fetcher from "../../lib/fetcher";
 
-const Posts = (props: {
-  posts: {
-    id: number;
-    thread: {
-      topic: string;
-    };
-    title: string;
-    author: {
-      name: string;
-      image: string;
-    };
-    content: string;
-    date: string;
-  }[];
-}) => {
+const Posts = () => {
   const router = useRouter();
+  const {
+    data: posts,
+    error,
+    isLoading,
+  } = useSWR("/api/posts", fetcher, {
+    refreshInterval: 5000,
+  });
 
   return (
     <Container>
@@ -37,44 +31,22 @@ const Posts = (props: {
         </Button>
       </Flex>
       <div className="mt-6 grid grid-cols-3 gap-4">
-        {props.posts.map((post, index) => {
-          return (
-            <ArticleCardVertical
-              key={index}
-              image="https://images.unsplash.com/photo-1602080858428-57174f9431cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80"
-              {...post}
-            />
-          );
-        })}
+        {!isLoading ? (
+          posts.map((post, index) => {
+            return (
+              <ArticleCardVertical
+                key={index}
+                image="https://images.unsplash.com/photo-1602080858428-57174f9431cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80"
+                {...post}
+              />
+            );
+          })
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </Container>
   );
 };
-
-export async function getServerSideProps() {
-  const posts = await new PrismaClient().posts.findMany({
-    select: {
-      id: true,
-      title: true,
-      author: {
-        select: {
-          name: true,
-          image: true,
-        },
-      },
-      thread: {
-        select: {
-          topic: true,
-        },
-      },
-    },
-  });
-
-  return {
-    props: {
-      posts,
-    },
-  };
-}
 
 export default Posts;
