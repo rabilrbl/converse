ce5db98567776fa1fef2ede5981b1ba44839b491
Mohammed Rabil <rabil@techie.com>
2023-01-17 00:13:29 +0530

    Rely on google calendar, add iframe

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,42 +1,13 @@
-import { useState } from "react";
-import { Calendar } from '@mantine/dates';
-
 const CalendarPage = () => {
-  const [value, setValue] = useState<Date | null>(new Date());
-
   return (
     <div>
-        <h1 className="font-extrabold">Calendar</h1>
-        <Calendar
-          value={value}
-          onChange={setValue}
-          fullWidth
-          size="xl"
-          styles={(theme) => ({
-            cell: {
-              border: `1px solid ${
-                theme.colorScheme === "dark"
-                  ? theme.colors.dark[4]
-                  : theme.colors.gray[2]
-              }`,
-            },
-            day: { borderRadius: 0, height: 70, fontSize: theme.fontSizes.lg },
-            weekday: { fontSize: theme.fontSizes.lg },
-            weekdayCell: {
-              fontSize: theme.fontSizes.xl,
-              backgroundColor:
-                theme.colorScheme === "dark"
-                  ? theme.colors.dark[5]
-                  : theme.colors.gray[0],
-              border: `1px solid ${
-                theme.colorScheme === "dark"
-                  ? theme.colors.dark[4]
-                  : theme.colors.gray[2]
-              }`,
-              height: 70,
-            },
-          })}
-        />
+      <h1 className="font-extrabold">Calendar</h1>
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=650&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FKolkata&src=Y18xNTVjODVhMWY4MmMxYTliNzk2YzRmNzQ5ZjM2MjRmOTcyYjI0M2ZlODU5NDRjNWVlMTA2ZDhmNWNiYmU4MDdkQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4uaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%237CB342&color=%230B8043"
+        style={{ border: "solid 1px #777" }}
+        width="1200"
+        height="650"
+      ></iframe>
     </div>
   );
 };
