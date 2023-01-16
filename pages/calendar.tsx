7219b2bf4999f291cb44226904b5e9dd3f7a4947
Mohammed Rabil <rabil@techie.com>
2023-01-17 01:02:18 +0530

    Add tailwind styling

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -4,11 +4,7 @@ const CalendarPage = () => {
       <h1 className="font-extrabold">Calendar</h1>
       <iframe
         src="https://calendar.google.com/calendar/embed?height=650&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FKolkata&hl=en_GB&title=SJEC%20Calendar&src=NDY1MjYxZDJiZjA2M2U2NjA0NGNhYjJlNGRhZTRhMzYxNGJhYjU5MzA3ZjQyN2ZiZGI1OTJmZDZiMGI5OTEwM0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%233F51B5&color=%230B8043"
-        width="1200"
-        height="650"
-        style={{
-          borderRadius: "10px",
-        }}
+        className="w-full h-[89vh] rounded-xl shadow-lg shadow-gray-400"
       ></iframe>
     </div>
   );
